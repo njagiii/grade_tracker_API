1973a65b1abd0c52da0822c542dc1239a0e51cd7
Njagi <ryannjagi2002@gmail.com>
2026-06-27 00:06:38 +0300

    Add models.py - This is where we describe our Database table

---
+++ models.py
@@ -0,0 +1,13 @@
+# Database table - describe our database table
+# We write python and SQLAlchemy does the rest by creating an sql table
+from sqlalchemy import Column, Integer, String, Float
+from database import Base
+
+class Student(Base): # creates pythone class student that inherits from base
+    __tablename__='students'
+
+    id = Column(Integer,primary_key=True, index=True) # id is a column in table and is an integer
+    name = Column(String, nullable=False) #nullable = false means the field is required
+    course = Column(String, nullable=False)
+    grade = Column(Float, default=0.0)
+    email = Column(String, unique=True)
